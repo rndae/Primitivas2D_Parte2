e8ad117da0a80b80f9da8e6fae8ac08607e625a0
cristhianzb <cristhianzentenob@gmail.com>
2017-05-06 23:07:25 -0400

    comentario

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
+//comentario
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
